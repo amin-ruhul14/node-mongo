863c1e99825d435a52d776e418d2d08658668cd0
aminruhul14 <ramin5849@gmail.com>
2020-12-06 04:30:57 +0600

    update dynamic API

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const users = ['Amin', 'Ruhul', 'Pabel', 'Rabbi', 'Hridoy', 'Yeasfi', 'Sohan']
+
 app.get('/', (req, res) => {
-    res.send("I am starting Node Module... YAY!");
+    const fruit = {
+        product: 'apple, banana',
+        price: '100, 80'
+    }
+    res.send(fruit);
 });
+app.get('/fruit/banana', (req, res) => {
+    res.send({ fruit: 'banana', quantity: 1000, price: 5000 });
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query.sort);
+    const name = users[id];
+    res.send({ id, name });
+})
 
 app.listen(8080, console.log("Listening port 8080"));
